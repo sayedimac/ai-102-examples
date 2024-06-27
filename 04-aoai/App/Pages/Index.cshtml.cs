90ed01a7e2522c069a89b251ebe857398342a0f5
Johan Myburgh <myburgh@gmail.com>
2024-06-27 21:51:30 +0100

    Merge branch 'main' of https://github.com/paulmsft/ai-102-examples

--- 04-aoai/App/Pages/Index.cshtml.cs
+++ 04-aoai/App/Pages/Index.cshtml.cs
@@ -27,41 +27,74 @@ public class IndexModel : PageModel
         _serviceDeployment = _configuration.GetValue<string>("DeploymentName") ?? "DEPLOYMENT NOT SET";
     }
 
-    public async Task<IActionResult> OnPostProcessInputAsync(string UserText, string SystemText)
+    public async Task<IActionResult> OnPostProcessInputAsync(string UserText, string SystemText, string myUserText, string assistantText, bool useFewShot, int temp)
     {
 
         // Process the input text
-        string userPrompt = UserText;
-        string systemPrompt = SystemText;
+        string userPrompt = UserText ?? "";
+        string systemPrompt = SystemText ?? "You are an AI chat bot answering general questions";
+        string myUserPrompt = myUserText ?? "";
+        string assistantPrompt = assistantText ?? "";
+        // bool useFewShot = useFewShot;
+        float temperature = temp;
 
         // Set the original text to output
         ViewData["systemMessage"] = systemPrompt;
         ViewData["userMessage"] = userPrompt;
+        ViewData["useFewShot"] = useFewShot;
+        ViewData["myUserMessage"] = myUserPrompt;
+        ViewData["assistantMessage"] = assistantPrompt;
 
 
         // Create a new OpenAI client
         OpenAIClient client = new OpenAIClient(new Uri(_serviceEndpoint), new AzureKeyCredential(_serviceKey));
-        
+
         // Create a new chat completions options object
-        var chatCompletionsOptions = new ChatCompletionsOptions()
+        if (useFewShot)
         {
-            Messages =
+
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages =
         {
             new ChatMessage(ChatRole.System, systemPrompt),
+            new ChatMessage(ChatRole.User, myUserPrompt),
+            new ChatMessage(ChatRole.Assistant, assistantPrompt),
             new ChatMessage(ChatRole.User, userPrompt)
         },
             Temperature = 0.5f,
             MaxTokens = 800,
             DeploymentName = _serviceDeployment
         };
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
 
+            ChatCompletions completions = response.Value;
+            string completion = completions.Choices[0].Message.Content;
+            ViewData["Completion"] = completion;
+            return Page();
+        }
+        else
+        {
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                Messages =
+        {
+            new ChatMessage(ChatRole.System, systemPrompt),
+            new ChatMessage(ChatRole.User, userPrompt)
+        },
+                Temperature = temperature / 100,
+                MaxTokens = 800,
+                DeploymentName = _serviceDeployment
+            };
+            Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
+
+            ChatCompletions completions = response.Value;
+            string completion = completions.Choices[0].Message.Content;
+            ViewData["Completion"] = completion;
+            return Page();
+
+        }
         // Get response from Azure OpenAI
-        Response<ChatCompletions> response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
-
-        ChatCompletions completions = response.Value;
-        string completion = completions.Choices[0].Message.Content;
-        ViewData["Completion"] = completion;
-        return Page();
     }
 
     public void OnGet()
